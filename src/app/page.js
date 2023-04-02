a407745dd78c30ba5f8bb0372ff9e57e1d239ff3
Asma <asmasiddiquemus06@gmail.com>
2023-04-02 14:35:11 +0500

    install tailwind css

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,6 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <h1 className='text-red-400 '>HOME</h1>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
