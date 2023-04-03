b167f22fcd5b9b081429783fd3dbfaef2fe19d5e
Asma <asmasiddiquemus06@gmail.com>
2023-04-03 17:45:43 +0500

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div className='max-w-6xl mx-auto space-y-4'>
+            <h1 className='mx-auto text-2xl font-medium text-amber-600'>About</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, autem! Commodi modi repellendus mollitia! Optio odit assumenda animi sapiente nihil? Ipsam architecto eligendi reiciendis impedit!</p>
+            <p >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque suscipit est, officia consectetur molestiae voluptatem. Ab, eveniet officia possimus sed sit, dolorem nostrum enim quidem illum facere repellendus quisquam</p>
+            <p>nihil rerum perspiciatis quis maiores sunt, optio quo veniam omnis culpa aliquid vel non placeat! Iure incidunt voluptates voluptas magnam corrupti quos animi. Reprehenderit, soluta odio, optio architecto quos sit, nesciunt nihil quis provident labore quo facere? Et perferendis, eum asperiores at dolores quibusdam suscipit ab ipsum similique, quae dolor officiis iure aut labore atque ipsa, omnis iusto culpa voluptatem! Neque mollitia autem alias dolorum exercitationem omnis nemo tenetur ut dolores.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quo laudantium officiis hic illum, commodi ipsum reiciendis quod amet et quas sunt fugiat voluptatem cum?</p>
+        </div>
+    );
+}
