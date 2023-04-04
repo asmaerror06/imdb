ed93badb557e432360b13bb20a6ab8ff5cd19b3a
Asma <asmasiddiquemus06@gmail.com>
2023-04-04 23:12:38 +0500

    add card component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
+import Card from './Card';
 
 export default function Results({ results }) {
     return (
-        <div>
+        <div className='max-w-6xl py-4 mx-auto sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-col-5'>
             {results.map((result) => (
-                <div key={result.id}>
-                    {result.original_title}
-                </div>
+                <Card key={result.id} result={result} />
+
             ))}
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/line-clamp')],
   darkMode: "class",
 }
 
