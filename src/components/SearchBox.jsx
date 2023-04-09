96edb966cb7cd4c519b56bdb7fd01fcdbdeff176
Asma <asmasiddiquemus06@gmail.com>
2023-04-09 22:50:39 +0500

    re-ease search box

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -17,7 +17,7 @@ export default function SearchBox() {
             <input value={search} type="text" placeholder='Search keyword...'
                 onChange={(e) => setSearch(e.target.value)}
                 className='flex-1 w-full placeholder-gray-500 bg-transparent rounded-sm outline-none h-14 ' />
-            <button disabled={!search} type='submit' className='text-amber-600 disabled:text-gray-400'></button>
+            <button disabled={!search} type='submit' className='text-amber-600 disabled:text-gray-400'>Search</button>
 
         </form>
     )
