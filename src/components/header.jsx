e586590806edc2613f20cb10100cc228eceabdaa
Asma <asmasiddiquemus06@gmail.com>
2023-04-09 23:04:19 +0500

    create the header section

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import MenuItem from './MenuItem'
+import React from 'react';
+import MenuItem from './MenuItem';
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
 import Link from 'next/link';
